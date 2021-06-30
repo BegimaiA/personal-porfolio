0cd16a3d29f3deaf42c9166f225838ad3764c242
Begimai Agybaeva <begimai.a@yandex.ru>
2021-06-30 18:53:40 +0600

    added animation

--- js/main.js
+++ js/main.js
@@ -53,3 +53,20 @@ $(document).ready(function(){
 
     });
 });
+var textWrapper = document.querySelector('.text-2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+    .add({
+        targets: '.text-2 .letter',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 2250,
+        delay: (el, i) => 150 * (i+1)
+    }).add({
+    targets: '.text-2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+});
